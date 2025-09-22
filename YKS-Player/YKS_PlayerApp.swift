85214cc44d347d6151e303f618316320711f64c8
hpq <663992442@qq.com>
2025-09-22 17:00:32 +0800

    Initial Commit

---
+++ YKS-Player/YKS_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YKS_PlayerApp.swift
+//  YKS-Player
+//
+//  Created by 水月空 on 2025/9/22.
+//
+
+import SwiftUI
+
+@main
+struct YKS_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YKS-PlayerTests/YKS_PlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  YKS_PlayerTests.swift
+//  YKS-PlayerTests
+//
+//  Created by 水月空 on 2025/9/22.
+//
+
+import Testing
+@testable import YKS_Player
+
+struct YKS_PlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
